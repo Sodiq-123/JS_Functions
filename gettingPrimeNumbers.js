d0f8785cf988f96b38714b7527999799d418491d
Sodiq_dev <57923180+Sodiq-123@users.noreply.github.com>
2020-11-13 13:31:22 +0100

    Create gettingPrimeNumbers.js

---
+++ gettingPrimeNumbers.js
@@ -0,0 +1,30 @@
+// First way
+
+function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+  
+      console.log( i ); // a prime
+    }
+  }
+  
+  // Second way
+  
+  function showPrimes(n) {
+
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+
+    alert(i);  // a prime
+  }
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if ( n % i == 0) return false;
+  }
+  return true;
+}
