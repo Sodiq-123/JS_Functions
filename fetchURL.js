8fade7e3d46f9f2ee0902c6130d3aa694563f17d
Sodiq_dev <57923180+Sodiq-123@users.noreply.github.com>
2021-08-05 11:22:43 +0100

    Update fetchURL.js

--- fetchURL.js
+++ fetchURL.js
@@ -1,1 +1,42 @@
+async function fetchURLs(urls) {
+    var results =  []
 
+    for (let url of urls) {
+        let res = await fetch(url)
+        if (res.status == 200) {
+            let text = await res.text()
+            results.push(text.toUpperCase())
+        } else {
+            results.push(`${res.status}: ${url}`)
+        }
+    }
+    return results
+}
+
+function *fetchURLs(urls) {
+    for (let url of urls) {
+        let res = await fetch(url)
+        if (res.status == 200) {
+            let text = await res.text()
+            yield text.toUpperCase()
+        } else {
+            yield `${res.status}: ${url}`
+        }
+    }
+}
+
+async function main(favoriteSites) {
+    var it = fetchURLs(favoriteSites)
+
+    while (true) {
+        let res = await it.next()
+        if (res.done) break
+        console.log(res.value)
+    }
+}
+
+async function main(favoriteSites) {
+    for await (let text of fetchURLs(favoriteSites)) {
+        console.log(text)
+    }
+}
